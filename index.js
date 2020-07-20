fe2fb7621421837a0e4444360285b6d17c86fc13
Renato A dos Santos <40146884+Git-RenatoAlcantara@users.noreply.github.com>
2020-07-20 13:53:20 -0300

    paypal_checkout
    Arquivo feito no video do meu canal pagamento com paypal 2.0

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-var http = require('http');
+const express = require("express");
+var bodyParser = require('body-parser');
+var paypal = require('paypal-rest-sdk');
 
-var server = http.createServer(function(request, response) {
+const app = express();
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AYmxz0Ofabf6JYI5GvHw1D50EAh0fDwXMS9EpDQaGJzT9KqyJNkILLBfjlT_gNrApy7jNKiHKIwTTn5z',
+  'client_secret': 'EFFqUYbQBRxOUJuWbqkqjQ7rDiBm2BfTIFwDQ9eFMBkprOBsfK9fxp-zzt6Snv7uWkW7sjK4G5lZLN5p'
 });
 
-var port = 80;
-server.listen(port);
+app.get('/', (req, res) => {
+    res.status(200).send({message: "Api Nodejs PaypalSDK"});
+});
 
-console.log("Server running at http://localhost:%d", port);
+app.post('/pay', (req, res)=> {
+    console.log(req.body);
+    var create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://return.url",
+            "cancel_url": "http://cancel.url"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "item",
+                    "sku": "item",
+                    "price": "1.00",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "USD",
+                "total": "1.00"
+            },
+            "description": "This is the payment description."
+        }]
+    };
+ 
+ 
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for(let i = 0; i < payment.links.length; i++ ){
+                if(payment.links[i].rel == 'approval_url'){
+                    res.redirect(payment.links[i].href);
+                    console.log(payment.links[i].href);
+                }
+            }
+        }
+    });
+
+});
+app.listen(process.env.PORT || 3000);
