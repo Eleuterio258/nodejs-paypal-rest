ed6f05cd5bb465c7fb89c946584f4ba29cd2bdd4
Renato A dos Santos <40146884+Git-RenatoAlcantara@users.noreply.github.com>
2020-07-29 20:50:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const express = require("express");
-var bodyParser = require('body-parser');
-var paypal = require('paypal-rest-sdk');
+const express    = require("express");
+const bodyParser = require("body-parser");
+var   fs       = require("fs");
+var   paypal     = require('paypal-rest-sdk');
+const path = require('path');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
- 
+const dirname = path.join(__dirname, '/html/pagamento.html');
 paypal.configure({
-  'mode': 'sandbox', //sandbox or live
-  'client_id': 'AYmxz0Ofabf6JYI5GvHw1D50EAh0fDwXMS9EpDQaGJzT9KqyJNkILLBfjlT_gNrApy7jNKiHKIwTTn5z',
-  'client_secret': 'EFFqUYbQBRxOUJuWbqkqjQ7rDiBm2BfTIFwDQ9eFMBkprOBsfK9fxp-zzt6Snv7uWkW7sjK4G5lZLN5p'
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'AYmxz0Ofabf6JYI5GvHw1D50EAh0fDwXMS9EpDQaGJzT9KqyJNkILLBfjlT_gNrApy7jNKiHKIwTTn5z',
+    'client_secret': 'EFFqUYbQBRxOUJuWbqkqjQ7rDiBm2BfTIFwDQ9eFMBkprOBsfK9fxp-zzt6Snv7uWkW7sjK4G5lZLN5p'
+  });
+  
+
+
+app.get('/', (req, res) =>{
+    res.status(200).send(JSON.stringify({message: "Api NodeJS com integração para pagamento com paypal", autor: "Renato A dos Santos"}));
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send({message: "Api Nodejs PaypalSDK"});
-});
-
-app.post('/pay', (req, res)=> {
+app.post('/pay', (req, res) => {
     console.log(req.body);
+
     var create_payment_json = {
         "intent": "sale",
         "payer": {
             "payment_method": "paypal"
         },
         "redirect_urls": {
-            "return_url": "http://return.url",
+            "return_url": "https://Paypal-Rest-Sdk.renatoalcantara.repl.co/success",
             "cancel_url": "http://cancel.url"
         },
         "transactions": [{
@@ -48,20 +50,84 @@ app.post('/pay', (req, res)=> {
             "description": "This is the payment description."
         }]
     };
- 
- 
-    paypal.payment.create(create_payment_json, function (error, payment) {
+
+
+    paypal.payment.create(create_payment_json, function(error, payment){
         if (error) {
             throw error;
         } else {
-            for(let i = 0; i < payment.links.length; i++ ){
-                if(payment.links[i].rel == 'approval_url'){
-                    res.redirect(payment.links[i].href);
-                    console.log(payment.links[i].href);
-                }
-            }
+             payment.links.forEach(links => {
+                 if(links.rel == 'approval_url'){
+                     res.status(200).send({link: links.href});
+                 }
+             })
+            // for(let i = 0; i <.length; i++){
+            //     if(payment.links[i].rel  == 'approval_url'){
+            //         res.status(200).send({link: payment.links[i].href});
+            //     }
+            // }
         }
     });
 
 });
+
+app.get('/success', (req, res) => {
+
+    var execute_payment_json = {
+        "payer_id": req.query.PayerID,
+        "transactions": [{
+            "amount": {
+                "currency": "USD",
+                "total": "1.00"
+            }
+        }]
+    }
+
+    var paymentId = req.query.paymentId;
+    var authid;
+    paypal.payment.execute(paymentId, execute_payment_json, function(error, payment){
+        console.log(JSON.stringify(payment));
+        if(error){
+            console.log(error.response);
+        }else{
+           
+           if (payment.state === 'approved'){
+                // Capture authorization.
+               
+
+                fs.readFile(dirname, (error, data) => {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(data)
+                })
+
+            } else {
+                console.log('payment not successful');
+            }
+        }
+    })
+
+    
+});
+
+
+app.get('/order', (req, res) => {
+    // Set capture details.
+    var capture_details = {
+        amount: {
+            "currency": "USD",
+            "total": "1.00"
+        },
+        is_final_capture: false
+    };
+         console.log(req.query.id);
+         paypal.order.get(req.query.id, capture_details,  (error, capture) =>{
+                if (error) {
+                    console.error(JSON.stringify(error));
+                } else {
+                    console.log(JSON.stringify(capture));
+                }
+            })
+    })
+
+
 app.listen(process.env.PORT || 3000);
